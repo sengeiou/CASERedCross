b76cad05bc4fb993d3cdff25a345a359dd945b9d
alucard263096 <alucard263096@126.com>
2019-05-05 11:00:46 +0800

    commit

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import { HTTP } from '@ionic-native/http/ngx';
+import { DBMgr } from 'src/mgr/DBMgr';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +15,19 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public sqlite: SQLite,
+    public http: HTTP
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      
+      DBMgr.GetInstance().Init(this.http, this.sqlite);
+
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
