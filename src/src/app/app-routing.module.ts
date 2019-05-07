a75ed957f532d17418c28162e89bf3ae9859f1e6
alucard263096 <alucard263096@126.com>
2019-05-07 12:46:48 +0800

    加了个demo

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'test', pathMatch: 'full' },
+  { path: '', redirectTo: 'chartdemo', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'test', loadChildren: './test/test.module#TestPageModule' },
   { path: 'phone', loadChildren: './phone/phone.module#PhonePageModule' },
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'visit', loadChildren: './visit/visit.module#VisitPageModule' },
   { path: 'visilt-list', loadChildren: './visilt-list/visilt-list.module#VisiltListPageModule' },
   { path: 'visit-record', loadChildren: './visit-record/visit-record.module#VisitRecordPageModule' },
+  { path: 'chartdemo', loadChildren: './chartdemo/chartdemo.module#ChartdemoPageModule' },
 
 
 
